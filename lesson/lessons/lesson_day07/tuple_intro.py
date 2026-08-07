b5c139189e9d6beb6e5a93fac4db2bf27e3b6c3e
Vinay Poswal <277703806+vinayposwal-oss@users.noreply.github.com>
2026-08-07 23:05:16 +0530

    day 07

---
+++ lesson/lessons/lesson_day07/tuple_intro.py
@@ -0,0 +1,7 @@
+# Tuples are immutable sequences, typically used to store collections of heterogeneous data. 
+# Tuples are defined by enclosing the elements in parentheses `()`.
+week = ("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
+
+print(week[1])
+print(week[-1])
+print(len(week))
---
+++ lesson/lessons/lesson_day07/tuple_vs_list.py
@@ -0,0 +1,6 @@
+fruits = ["mango", "banana", "grapes", "apple"]
+fruits.append("kiwi")
+#list is mutable, we can change the values of list
+
+months = ("January", "February", "March", "April", "May") 
+months.append("June") # This will raise an AttributeError since tuples are immutable
